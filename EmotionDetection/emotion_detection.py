a7391feffc1e33d312ec2411c39562b5c43f6db3
alishersv <alishersv@gmail.com>
2025-01-24 12:38:11 -0500

    Task 7 done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,31 +6,41 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers=header, json=input_json, timeout=2)
-    formatted_response = json.loads(response.text)
     
-    predictions = formatted_response['emotionPredictions'][0]
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        predictions = formatted_response['emotionPredictions'][0]
+        anger_score = predictions['emotion']['anger']
+        disgust_score = predictions['emotion']['disgust']
+        fear_score = predictions['emotion']['fear']
+        joy_score = predictions['emotion']['joy']
+        sadness_score = predictions['emotion']['sadness']
 
-    anger_score = predictions['emotion']['anger']
-    disgust_score = predictions['emotion']['disgust']
-    fear_score = predictions['emotion']['fear']
-    joy_score = predictions['emotion']['joy']
-    sadness_score = predictions['emotion']['sadness']
+        max_score = max(return_response.values())
+        dominant_emotion = ""
+
+        for key, value in return_response.items():
+            if value == max_score:
+                dominant_emotion = key
+                break
+
+        return_response['dominant_emotion'] = dominant_emotion
+    
+    if response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
 
     return_response = {
         "anger": anger_score,
         "disgust": disgust_score,
         "fear": fear_score,
         "joy": joy_score,
-        "sadness": sadness_score
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
         }
 
-    max_score = max(return_response.values())
-    dominant_emotion = ""
-
-    for key, value in return_response.items():
-        if value == max_score:
-            dominant_emotion = key
-            break
-
-    return_response['dominant_emotion'] = dominant_emotion
     return return_response
--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("EmotionDetection")
+app = Flask("Emotion Detection")
 
 @app.route('/emotionDetector')
 def return_emotion():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    if response['dominant_emotion'] == None:
+        return "Invalid text! Please try again!."
+    else:
+        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
 
 @app.route('/')
 def render_index_page():
-    return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
